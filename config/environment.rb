3562231d10977e14adde6a6f95cf67ca5e24439a
md-farhan-memon <md.farhanmemon@gmail.com>
2017-01-28 03:17:04 +0530

    bug fix

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,13 @@ Rails.application.initialize!
 
 # Mailer
 
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
+  openssl_verify_mode: 'none',
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
 }
